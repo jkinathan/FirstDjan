5d5f21271ca68d3bc9790267787380eb74bd3773
jkinobe <jkinobe@gmail.com>
2020-04-02 14:06:50 +0300

    django forms

--- MyDjango/first_project/first_app/urls.py
+++ MyDjango/first_project/first_app/urls.py
@@ -3,5 +3,5 @@ from first_app import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    
+    url(r'^formspage',views.form_view_name,name='My form'),
 ]
--- MyDjango/first_project/first_app/views.py
+++ MyDjango/first_project/first_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Webpage, Topic, Accessrecord
+from . import forms
 # Create your views here.
 
 def index(request):
     webpages_list = Accessrecord.objects.order_by('date')
     date_dict = {'access_records' : webpages_list}
     return render(request, 'first_app/index.html',context=date_dict)
+
+def form_view_name(request):
+    form = forms.FormName()#creating a new instance of the formName we created in forms.py and store it in form now called
+    
+    if request.method == "POST":
+        form = forms.FormName(request.POST)
+        
+        if form.is_valid():
+            #do something
+            print("Validation success! ")
+            print(form.cleaned_data['name'])
+            print(form.cleaned_data['email'])
+    
+    return render(request, "first_app/form_page.html", {'myform':form})
+    
