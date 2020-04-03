4eb58f74fcf2658c0b94c0f81aa0e3a62b2c259b
jkinobe <jkinobe@gmail.com>
2020-04-03 12:52:54 +0300

    Form validations complete

--- MyDjango/first_project/first_app/forms.py
+++ MyDjango/first_project/first_app/forms.py
@@ -8,14 +8,17 @@ from django.core import validators
 class FormName(forms.Form):
     name = forms.CharField()#validators=[check_for_z]
     email = forms.EmailField()
-    text = forms.CharField(widget = forms.Textarea)
     verifyemail =forms.EmailField(label="Enter your Email again!!!")
+    text = forms.CharField(widget = forms.Textarea)
     
-    
-    def clean(self):
-        all_clean_data = super().clean()
-        email = all_clean_data['email']
+    #python 3 explicitly
+    # def clean(self):
+    #     all_clean_data = super().clean()
+    #     email = all_clean_data['email']
+    #     vemail = all_clean_data['verifyemail']
         
+    #     if email != vemail:
+    #         raise forms.ValidationError("Make sure both emails match")
     # botcatcher = forms.CharField(required=False, widget = forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
     
     # def clean_botcatcher(self):
