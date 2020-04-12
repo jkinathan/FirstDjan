68f8951b45542ae9bc4950063c7decacf00c244e
jkinobe <jkinobe@gmail.com>
2020-04-12 19:24:50 +0300

    URLS for CBVs

--- MyDjango/first_project/first_app/urls.py
+++ MyDjango/first_project/first_app/urls.py
@@ -7,7 +7,9 @@ from first_app import views
 #so here i removed the app_name = 'firstapp' from this urls.py in first_app  please check the project urls.py for the change
 #edited traditional path links to url links
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    #now we have to start calling the CBV class
+    #like url(r'^$',views.CBView.as_view()),
+    url(r'^$',views.CBView.as_view()),
     url(r'^formspage',views.form_view_name,name='Myform'),
     url(r'^relative/$', views.relative,name='relname'),#this is the page for testing relative paths in django
     url(r'^register/$',views.register,name='register'),
